82c36266bb8a8616d27a30f66a8488ae0138153b
Maciej Sobkowski <maciejjo@aristotle.(none)>
2014-01-16 01:13:16 +0100

    wykres q_0

---
+++ report/plot_qinit.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+  fn=$(basename $1)
+  echo $fn
+  f="${fn%.*}"
+
+  gnuplot << EOF
+  set terminal pdf enhanced
+  set datafile separator ","
+  set output "figures/${f}_smooth.pdf"
+  set grid
+  set xlabel "Liczba iteracji"
+  set ylabel "Cmax"
+  plot \
+        'plot_input/${f}.csv' using 1:2 title 'q_0 = 0.0' smooth csplines with lines, \
+        'plot_input/${f}.csv' using 1:3 title 'q_0 = 0.25' smooth csplines with lines, \
+        'plot_input/${f}.csv' using 1:4 title 'q_0 = 0.5' smooth csplines with lines, \
+        'plot_input/${f}.csv' using 1:5 title 'q_0 = 0.75' smooth csplines with lines, \
+        'plot_input/${f}.csv' using 1:6 title 'OPT' smooth csplines with lines
+
+  set output "figures/${f}.pdf"
+  plot \
+        'plot_input/${f}.csv' using 1:2 title 'q_0 = 0.0' with lines, \
+        'plot_input/${f}.csv' using 1:3 title 'q_0 = 0.25' with lines, \
+        'plot_input/${f}.csv' using 1:4 title 'q_0 = 0.5' with lines, \
+        'plot_input/${f}.csv' using 1:5 title 'q_0 = 0.75' with lines, \
+        'plot_input/${f}.csv' using 1:6 title 'OPT' with lines
+EOF
